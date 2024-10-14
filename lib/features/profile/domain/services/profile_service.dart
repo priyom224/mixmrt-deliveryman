45ac094d38e0b253a612a3b6cfa8413a7b6e5e0e
Shahidullah191 <shahidullah.cse.gub@gmail.com>
2024-10-14 22:56:42 +0600

    location permission issue fix

--- lib/features/profile/domain/services/profile_service.dart
+++ lib/features/profile/domain/services/profile_service.dart
@@ -60,18 +60,17 @@ class ProfileService implements ProfileServiceInterface {
   void checkPermission(Function callback) async {
     LocationPermission permission = await Geolocator.requestPermission();
     permission = await Geolocator.checkPermission();
-    if(permission == LocationPermission.denied
-        || (GetPlatform.isIOS ? false : permission == LocationPermission.whileInUse)) {
+    if(permission == LocationPermission.denied /*|| (GetPlatform.isIOS ? false : permission == LocationPermission.whileInUse)*/) {
       Get.dialog(CustomAlertDialogWidget(description: 'you_denied'.tr, onOkPressed: () async {
         Get.back();
         await Geolocator.requestPermission();
         checkPermission(callback);
       }), barrierDismissible: false);
-    }else if(permission == LocationPermission.deniedForever) {
-      Get.dialog(CustomAlertDialogWidget(description: 'you_denied_forever'.tr, onOkPressed: () async {
+    }else if(permission == LocationPermission.deniedForever || (GetPlatform.isIOS ? false : permission == LocationPermission.whileInUse)) {
+      Get.dialog(CustomAlertDialogWidget(description: permission == LocationPermission.whileInUse ? 'you_denied'.tr : 'you_denied_forever'.tr, onOkPressed: () async {
         Get.back();
         await Geolocator.openAppSettings();
-        checkPermission(callback);
+        // checkPermission(callback);
       }), barrierDismissible: false);
     }else {
       callback();
